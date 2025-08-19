c951e4abfee83575ab9667e8afb17855387ab34b
Aditya Bharde <adityabharde05@gmail.com>
2025-08-20 00:49:39 +0530

    Time: 0 ms (100%), Space: 40.5 MB (53.38%) - LeetHub

---
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int xor=0;
+        int arr []= new int[n];
+        for(int i =0 ;i<n;i++){
+            arr[i]=start + 2 * i;
+        }
+        for(int a : arr){
+            xor^=a;
+        }
+        return xor;
+    }
+}
