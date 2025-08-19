e5885d83dcc632196842d5d37b127074913a4bba
Aditya Bharde <adityabharde05@gmail.com>
2025-08-20 00:45:33 +0530

    Time: 1 ms (100%), Space: 42.5 MB (23.34%) - LeetHub

---
+++ 3312-number-of-changing-keys/3312-number-of-changing-keys.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countKeyChanges(String s) {
+        int changes = 0;
+        for (int i = 1; i < s.length(); i++) {
+            char prev = Character.toLowerCase(s.charAt(i - 1));
+            char curr = Character.toLowerCase(s.charAt(i));
+            if (prev != curr) {
+                changes++;
+            }
+        }
+        return changes;
+    }
+}
